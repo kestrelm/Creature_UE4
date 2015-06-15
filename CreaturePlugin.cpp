87ff36887840e91188aa239481391f881229dc99
Kestrel Moon <creature@kestrelmoon.com>
2015-06-15 16:06:54 -0700

    Adding Plugins code

---
+++ CreaturePlugin.cpp
@@ -0,0 +1,12 @@
+#include "CustomProceduralMesh.h"
+#include "CreaturePlugin.h"
+
+void CreaturePlugin::StartupModule()
+{
+}
+
+void CreaturePlugin::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(CreaturePlugin, Module)
