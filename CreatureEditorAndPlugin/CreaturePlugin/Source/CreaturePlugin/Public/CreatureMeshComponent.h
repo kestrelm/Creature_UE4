11e7dd4f86aa5400dff2e3735fb30dd136cc4309
kestrelm <creature@kestrelmoon.com>
2016-09-23 16:33:14 -0700

    Revert "Removing accuracy, changing trigger calculation"
    This reverts commit 2e4d49d8d3460127c7e1c15dceb54fad4f27b1ee.

--- CreatureEditorAndPlugin/CreaturePlugin/Source/CreaturePlugin/Public/CreatureMeshComponent.h
+++ CreatureEditorAndPlugin/CreaturePlugin/Source/CreaturePlugin/Public/CreatureMeshComponent.h
@@ -165,7 +165,7 @@ USTRUCT()
 struct FCreatureFrameCallback {
 	GENERATED_USTRUCT_BODY()
 	FCreatureFrameCallback()
-		: triggered(false)
+		: triggered(false), accuracy(1.5f)
 	{}
 
 	void resetCallback(float frameIn)
@@ -176,16 +176,16 @@ struct FCreatureFrameCallback {
 
 	bool tryTrigger(float frameIn)
 	{
-		auto oldFrame = (float)currentFrame;
 		currentFrame = (int32)frameIn;
 
 		if (triggered)
 		{
 			return false;
 		}
+
+		auto frame_diff = fabs(frameIn - (float)frame);
 		
-		auto triggerFrame = (float)frame;
-		if(triggerFrame > oldFrame && triggerFrame <= currentFrame)
+		if(frame_diff <= accuracy)
 		{
 			triggered = true;
 			return true;
@@ -205,6 +205,10 @@ struct FCreatureFrameCallback {
 	/** Frame to trigger event*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|Creature")
 	int32 frame;
+
+	/** What frame difference is required before the trigger is fired. A recommended value is 1.0 to 1.5 frame difference */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components|Creature")
+	float accuracy;
 
 	bool triggered;
 	int32 currentFrame;
