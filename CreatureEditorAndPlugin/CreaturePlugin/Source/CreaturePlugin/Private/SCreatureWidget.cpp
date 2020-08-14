542c60d923188a6b275d432e8e171f356f7de524
kestrelm <creature@kestrelmoon.com>
2020-08-14 16:18:44 +0800

    Checking to see if creature_manager is null for SCreatureWidget

--- CreatureEditorAndPlugin/CreaturePlugin/Source/CreaturePlugin/Private/SCreatureWidget.cpp
+++ CreatureEditorAndPlugin/CreaturePlugin/Source/CreaturePlugin/Private/SCreatureWidget.cpp
@@ -36,6 +36,11 @@ void SCreatureWidget::UpdateMesh(const FVector2D& translation, const FVector2D& 
 		return;
 	}
 
+	if (creature_core->creature_manager == nullptr)
+	{
+		return;
+	}
+
 	auto proc_mesh = creature_core->GetProcMeshData(world_type);
 	creature_core->UpdateCreatureRender();
 
